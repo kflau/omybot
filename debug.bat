4d9cbd38d5ad581d33708459a21a84572df9ebe6
nelson.lau <nelson.kf.lau@gmail.com>
2018-04-19 19:24:10 +0800

    Update HKEX token

--- debug.bat
+++ debug.bat
@@ -1,1 +1,1 @@
-dlv debug -- -token NDM1NzI3OTMxMjk4MjgzNTIx.DbhCZw.5ajrRN0freT8ifKUPJihfngquUg -webhook https://discordapp.com/api/webhooks/435474985235120128/Bx6gCR8E8X9DjNxSvS5YfTHIfVRF9W17q-TK3toa4SigTgOoDjjdV2bq3wvPsqMxW1yk -hkexToken evLtsLsBNAUVTPxtGqVeG9B8MG+J3ol24O59BU10wpi6NUJBeKz0Uz2Zu+SElSqf
+dlv debug -- -token NDM1NzI3OTMxMjk4MjgzNTIx.DbhCZw.5ajrRN0freT8ifKUPJihfngquUg -webhook https://discordapp.com/api/webhooks/435474985235120128/Bx6gCR8E8X9DjNxSvS5YfTHIfVRF9W17q-TK3toa4SigTgOoDjjdV2bq3wvPsqMxW1yk -hkexToken evLtsLsBNAUVTPxtGqVeG6V9RQvdOc+dvgoo+tbqYMQ+dUr84O7ZugDgeeAZ6kjX
