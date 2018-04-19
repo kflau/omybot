a99fd7a204d9d30933efb5bbfea9dbf3e1c30733
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-04-19 23:09:43 +0800

    Added bash commands

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+%GOPATH%/bin/omybot \
+	-botName OMyBot \
+	-token NDM1NzI3OTMxMjk4MjgzNTIx.DbhCZw.5ajrRN0freT8ifKUPJihfngquUg \
+	-webhook https://discordapp.com/api/webhooks/435474985235120128/Bx6gCR8E8X9DjNxSvS5YfTHIfVRF9W17q-TK3toa4SigTgOoDjjdV2bq3wvPsqMxW1yk \
+	-hkexToken evLtsLsBNAUVTPxtGqVeG6V9RQvdOc+dvgoo+tbqYMQ+dUr84O7ZugDgeeAZ6kjX
