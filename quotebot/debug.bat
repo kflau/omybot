cdc47d48ecf0cebfe12548381deb7be629bae191
nelson.lau <nelson.kf.lau@gmail.com>
2018-04-24 13:49:48 +0800

    Reformat

--- quotebot/debug.bat
+++ quotebot/debug.bat
@@ -1,4 +1,1 @@
-call dlv debug -- \
-	-botName QuoteBot \
-	-token NDM3NDkyMjQzNTQ1MTk0NTA4.Db22QA.Po1WbA7qB24zW53Fhd1R6BDIUwA \
-	-webhook https://discordapp.com/api/webhooks/435474985235120128/Bx6gCR8E8X9DjNxSvS5YfTHIfVRF9W17q-TK3toa4SigTgOoDjjdV2bq3wvPsqMxW1yk 
+call dlv debug -- -botName QuoteBot -token NDM3NDkyMjQzNTQ1MTk0NTA4.Db22QA.Po1WbA7qB24zW53Fhd1R6BDIUwA -webhook https://discordapp.com/api/webhooks/435474985235120128/Bx6gCR8E8X9DjNxSvS5YfTHIfVRF9W17q-TK3toa4SigTgOoDjjdV2bq3wvPsqMxW1yk 
